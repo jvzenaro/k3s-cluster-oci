8fd358dc3eb35d6172ae82cbdf92c1b7d90026db
João Zenaro <joao@zenaro.me>
2021-08-23 22:40:21 -0300

    Add more outputs for show ips

--- output.tf
+++ output.tf
@@ -1,6 +1,18 @@
-output "master_public_ip" {
+output "cluster_master_public_ip" {
   value = oci_core_instance.cluster-master-node01.public_ip
 }
+
+output "cluster_private_ips" {
+  value = [oci_core_instance.cluster-master-node01.private_ip,
+    oci_core_instance.cluster-master-node02.private_ip,
+    oci_core_instance.cluster-worker-node01.private_ip,
+  oci_core_instance.cluster-worker-node02.private_ip]
+}
+
+output "mysql_private_ip" {
+  value = oci_core_instance.k3s-mysql.private_ip
+}
+
 
 output "vcn_id" {
   value = oci_core_vcn.k3s_vcn.id
